8224b05eb284d427ff4b8c7d8798570fc3038a3f
Dimitar Dimitrov <dimitar.dimitrov@students.olin.edu>
2014-10-13 00:56:48 -0400

    Encoder! Also, some dude's code

--- Lab3/Lab3.ino
+++ Lab3/Lab3.ino
@@ -1,87 +1,71 @@
-*working variables*/
-unsigned long lastTime;
-double Input, Output, Setpoint;
-double ITerm, lastInput;
-double kp, ki, kd;
-int SampleTime = 1000; //1 sec
-double outMin, outMax;
-bool inAuto = false;
- 
-#define MANUAL 0
-#define AUTOMATIC 1
- 
-void Compute()
-{
-   if(!inAuto) return;
-   unsigned long now = millis();
-   int timeChange = (now - lastTime);
-   if(timeChange>=SampleTime)
-   {
-      /*Compute all the working error variables*/
-      double error = Setpoint - Input;
-      ITerm+= (ki * error);
-      if(ITerm> outMax) ITerm= outMax;
-      else if(ITerm< outMin) ITerm= outMin;
-      double dInput = (Input - lastInput);
- 
-      /*Compute PID Output*/
-      Output = kp * error + ITerm- kd * dInput;
-      if(Output> outMax) Output = outMax;
-      else if(Output < outMin) Output = outMin;
- 
-      /*Remember some variables for next time*/
-      lastInput = Input;
-      lastTime = now;
-   }
+int Strips=48;
+
+int Position=0;
+float Velocity=0;
+float Acceleration=0;
+int count=0;
+int debounce=0;
+int old_colour=0;
+int colour=0;
+int last_change=0;
+
+String inputString="";
+String cmd="";
+int val=0;
+boolean StringCompleate=false;
+
+void setup() {
+    old_colour=convert(analogRead(A0));
+    last_change=millis()
+
+    Serial.begin(9600);
+    while (!Serial) {;} //some Arduinos take a while...
+    inputString.reserve(128); //allows adding one byte at a time (it needs to be a buffer of sorts)
 }
- 
-void SetTunings(double Kp, double Ki, double Kd)
-{
-  double SampleTimeInSec = ((double)SampleTime)/1000;
-   kp = Kp;
-   ki = Ki * SampleTimeInSec;
-   kd = Kd / SampleTimeInSec;
+
+void loop() {
+    encoder();
+    if(StringCompleate) {
+        Serial.print("Position: ");
+        Serial.print(Position);
+        Serial.print("  Velocity: ");
+        Serial.print(Velocity);
+        Serial.print( "Acceleration: ");
+        Serial.println(Acceleration);
 }
- 
-void SetSampleTime(int NewSampleTime)
-{
-   if (NewSampleTime > 0)
-   {
-      double ratio  = (double)NewSampleTime
-                      / (double)SampleTime;
-      ki *= ratio;
-      kd /= ratio;
-      SampleTime = (unsigned long)NewSampleTime;
-   }
-}
- 
-void SetOutputLimits(double Min, double Max)
-{
-   if(Min > Max) return;
-   outMin = Min;
-   outMax = Max;
-    
-   if(Output > outMax) Output = outMax;
-   else if(Output < outMin) Output = outMin;
- 
-   if(ITerm> outMax) ITerm= outMax;
-   else if(ITerm< outMin) ITerm= outMin;
-}
- 
-void SetMode(int Mode)
-{
-    bool newAuto = (Mode == AUTOMATIC);
-    if(newAuto && !inAuto)
-    {  /*we just went from manual to auto*/
-        Initialize();
+
+void serialEvent() { //This gets run every time after loop
+    while (Serial.available()) {
+        char inChar = (char)Serial.read();
+        if (inChar == '\n') {
+            inputString.trim(); //Get rid of the \n
+            stringComplete = true; //The loop will run next time!
+        }
+        else { //Put all the input bytes in a buffer
+            inputString += inChar;
+        }
     }
-    inAuto = newAuto;
 }
- 
-void Initialize()
-{
-   lastInput = Input;
-   ITerm = Output;
-   if(ITerm> outMax) ITerm= outMax;
-   else if(ITerm< outMin) ITerm= outMin;
+
+
+void encoder() {
+    colour=convert(analogRead(A0));
+    
+    if(colour!=old_colour) {debounce++;}
+    else {debounce=0;}
+    
+    if(debounce>2) {
+        debounce=0;
+        if ((colour-old_colour)%3)==1) {count++%Strips;}
+        else {count--%Strips;}
+        old_colour=colour;
+        Position=count*360/Strips;
+        Velocity=(360/Strips)/(millis()-last_change);
+        last_change=millis()
+}
+
+int convert(raw) {
+    if(raw<256) {return 0;}
+    else if(raw>767) {return 2;}
+    else {return 1;}
 }
